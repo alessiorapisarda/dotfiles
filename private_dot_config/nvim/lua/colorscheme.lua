1caee2e6860990a9ba0953b0b89e86220cbf075b
Alessio Rapisarda <alessiorapisarda@tutanota.com>
2022-03-04 16:12:11 +0100

    Change line-ending to Unix

--- private_dot_config/nvim/lua/colorscheme.lua
+++ private_dot_config/nvim/lua/colorscheme.lua
@@ -1,9 +1,5 @@
 vim.opt.termguicolors = true
 
-local nightfox_status_ok, nightfox = pcall(require, "nightfox")
-if not nightfox_status_ok then
-    return
-end
 local rosepine_status_ok, p = pcall(require, "rose-pine.palette")
 if not rosepine_status_ok then
     return
@@ -23,28 +19,6 @@ if not monokai_status_ok then
     return
 end
 
--- Nightfox
--- nightfox.setup({
---   fox = "nightfox", -- Which fox style should be applied
---   transparent = false, -- Disable setting the background color
---   alt_nc = false, -- Non current window bg to alt color see `hl-NormalNC`
---   terminal_colors = true, -- Configure the colors used when opening :terminal
---   styles = {
---     comments = "NONE", -- Style that is applied to comments: see `highlight-args` for options
---     functions = "NONE", -- Style that is applied to functions: see `highlight-args` for options
---     keywords = "NONE", -- Style that is applied to keywords: see `highlight-args` for options
---     strings = "NONE", -- Style that is applied to strings: see `highlight-args` for options
---     variables = "NONE", -- Style that is applied to variables: see `highlight-args` for options
---   },
---   inverse = {
---     match_paren = false, -- Enable/Disable inverse highlighting for match parens
---     visual = false, -- Enable/Disable inverse highlighting for visual selection
---     search = false, -- Enable/Disable inverse highlights for search highlights
---   },
---   colors = {}, -- Override default colors
---   hlgroups = {}, -- Override highlight groups
--- })
---
 -- Rose Pine
 -- Set theme variant
 -- Matches terminal theme if unset
--- private_dot_config/nvim/lua/plugins/null-ls.lua
+++ private_dot_config/nvim/lua/plugins/null-ls.lua
@@ -5,7 +5,6 @@ end
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.stylua.with({ extra_args = { "--indent-type", "Spaces" } }),
         null_ls.builtins.formatting.rustfmt,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.prettierd,
